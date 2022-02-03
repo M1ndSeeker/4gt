6c14b0418f3c3902ea36e1ffbde660ddde9ba46a
Uladzislau Tsemenka <wladyslaw.tsemenko@gmail.com>
2022-02-03 11:51:38 +0300

    start pr

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,54 @@
 const gulp = require('gulp');
 const del = require('del');
-const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+const gcmq = require('gulp-group-css-media-queries');
 const sourcemaps = require('gulp-sourcemaps');
 const gulpIf = require('gulp-if');
+const less = require('gulp-less');
 const browserSync = require('browser-sync').create();
 
-let sourceMaps = process.argv.includes('--map');
-let minifyCss = process.argv.includes('--minify');
+let isSync = process.argv.includes('--sync');
+let isMap = process.argv.includes('--map');
+let isMinify = process.argv.includes('--minify');
 
 function clean() {
     return del('./app/*');
 }
 
 function styles() {
-	return gulp.src('./src/css/**/*.css')
-				.pipe(gulpIf(sourceMaps, sourcemaps.init()))
-				.pipe(concat('style.css'))
+	return gulp.src('./src/css/style.less')
+				.pipe(gulpIf(isMap,sourcemaps.init()))
+				.pipe(less())
 				.pipe(autoprefixer({}))
-				.pipe(gulpIf(minifyCss, cleanCSS({
+				.pipe(gcmq())
+				.pipe(gulpIf(isMinify, cleanCSS({
 					level: 2
 				})))
-				.pipe(gulpIf(sourceMaps, sourcemaps.write()))
+				.pipe(gulpIf(isMap,sourcemaps.write()))
 				.pipe(gulp.dest('./app/css'))
-				.pipe(browserSync.stream());
+				.pipe(gulpIf(isSync, browserSync.stream()));
+};
+
+function pictures() {
+	return gulp.src('./src/img/**/*')
+				.pipe(gulp.dest('./app/img'));
 };
 
 function watch() {
-	browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-	gulp.watch('./src/css/**/*.css', styles);
+	if (isSync) {
+		browserSync.init({
+			server: {
+				baseDir: "./"
+			}
+		});
+	}
+	gulp.watch('./src/css/**/*.less', styles);
 	gulp.watch("./*.html").on('change', browserSync.reload);
 }
 
-let buildWithClean = gulp.series(clean, styles);
+let build = gulp.parallel(styles, pictures);
+let buildWithClean = gulp.series(clean, build);
 let dev = gulp.series(buildWithClean, watch);
 
 gulp.task('build', buildWithClean);
